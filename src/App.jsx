35cfa54411fcb81fc7ff746f094b893efbf2550d
justinnotoma <justinhot906@gmail.com>
2024-02-24 23:46:14 -0500

    Code to add users

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [reminderState, setReminderState] = useState(true) // When 'reminderState' is set to true, then "WrapperReminder" is shown. When 'reminderState' is set to false, then "WrapperReminder" is hidden
   const [addUserState, setAddUserState] = useState(false) // When 'addUserState' is set to true, then "WrapperAddUser" is shown. When 'addUserState' is set to false, then "WrapperAddUser" is hidden
 
-  const [users, setUser] = useState([{'name': 'Justin', 'age': '17', 'profile': 'J', 'isBirthday': true}])
+  const [users, setUser] = useState([{'name': 'Justin', 'age': '17', 'profile': 'J', 'birthday': '2024-02-24', 'isBirthday': false}])
 
   const hideReminder = () => {
     setReminderState(false)
@@ -22,8 +22,39 @@ function App() {
     setUser([])
   }
 
+  const addUser = (name, date) => {
+    // Getting the user profile
+    const nameSpit = name.split('')
+    const profile = nameSpit[0].toUpperCase()
+
+    // Getting the user age
+    const currentDate = new Date
+    const currentYear = currentDate.getFullYear()
+
+    const dateParts = date.split('-')
+    let birthYear = dateParts[0]
+
+    let age = currentYear - birthYear
+
+    // Check to see if it is the user birthday
+    const currentMonth = ()  => {
+      let currentMonth = currentDate.getMonth()
+      currentMonth = currentMonth + 1
+
+      if(currentMonth < 10) currentMonth = `0${currentMonth}`
+      return currentMonth
+    }
+    const isBirthday = `${dateParts[1]}-${dateParts[2]}` === `${currentMonth()}-${currentDate.getDate()}`
+    console.log(isBirthday)
+    
+    // Add user to "users" array
+    setUser([...users, {'name': name, 'profile': profile, 'age': age, 'isBirthday': isBirthday}])
+    console.log(users)
+
+  }
+
   return (
-      (reminderState && !addUserState ? (<WrapperReminder hideWrapper={hideReminder} clearAll={clearAll} listOfUsers={users}/>): (<WrapperAddUser hideWrapper={hideAddUser}/>)) 
+      (reminderState && !addUserState ? (<WrapperReminder hideWrapper={hideReminder} clearAll={clearAll} listOfUsers={users}/>): (<WrapperAddUser hideWrapper={hideAddUser} addUser={addUser}/>)) 
   )
 }
 
--- src/components/WrapperAddUser.jsx
+++ src/components/WrapperAddUser.jsx
@@ -1,17 +1,27 @@
 import React, { useState } from 'react'
 
-export const WrapperAddUser = ({hideWrapper}) => {
+export const WrapperAddUser = ({hideWrapper, addUser}) => {
+    const [name, setName] = useState('')
+    const [birthday, setBirthday] = useState('')
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        addUser(name, birthday)
+
+        hideWrapper()
+    }
+
   return (
     <div className='wrapper-add-user'>
         <h1 className='font-400'>Add birthday</h1>
 
-        <form>
-            <input type="text" placeholder='Type user name' className='text-box font-500' id="" />
-            <input type="date" name="" className='text-box font-500' id="" />
-            <button type="submit" className='clear-btn'>Add birthday</button>
+        <form id='birthday-form' onSubmit={handleSubmit}>
+            <input type="text" placeholder='Type user name' className='text-box font-500' id="" onChange={(e) => setName(e.target.value)} />
+            <input type="date" name="" className='text-box font-500' id="" onChange={(e) => setBirthday(e.target.value)}/>
+            <button type="submit" className='clear-btn font-500'>Add birthday</button>
         </form>
 
-        <button className='clear-btn' onClick={() => hideWrapper()}>Close</button>
+        <button className='clear-btn font-500' onClick={() => hideWrapper()}>Close</button>
     </div>
   )
 }
