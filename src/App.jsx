25c85f2463ef40c307be514ba43e843684c51a95
justinnotoma <justinhot906@gmail.com>
2024-02-24 22:56:40 -0500

    Working Clear All button. Show users onscreen

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ function App() {
   const [reminderState, setReminderState] = useState(true) // When 'reminderState' is set to true, then "WrapperReminder" is shown. When 'reminderState' is set to false, then "WrapperReminder" is hidden
   const [addUserState, setAddUserState] = useState(false) // When 'addUserState' is set to true, then "WrapperAddUser" is shown. When 'addUserState' is set to false, then "WrapperAddUser" is hidden
 
+  const [users, setUser] = useState([{'name': 'Justin', 'age': '17', 'profile': 'J', 'isBirthday': true}])
+
   const hideReminder = () => {
     setReminderState(false)
     setAddUserState(true)
@@ -16,9 +18,12 @@ function App() {
     setReminderState(true)
   }
 
-  
+  const clearAll = () => {
+    setUser([])
+  }
+
   return (
-      (reminderState && !addUserState ? (<WrapperReminder hideWrapper={hideReminder}/>): (<WrapperAddUser hideWrapper={hideAddUser}/>)) 
+      (reminderState && !addUserState ? (<WrapperReminder hideWrapper={hideReminder} clearAll={clearAll} listOfUsers={users}/>): (<WrapperAddUser hideWrapper={hideAddUser}/>)) 
   )
 }
 
--- src/components/WrapperReminder.jsx
+++ src/components/WrapperReminder.jsx
@@ -1,22 +1,35 @@
 import React, { useState } from 'react'
 
-export const WrapperReminder = ({hideWrapper}) => {
+export const WrapperReminder = ({hideWrapper, clearAll, listOfUsers}) => {
     const [birthdayCount, setBirthdayCount] = useState(0)
+    
     return (
         <div className='wrapper-reminder'>
             <h1 className='font-400'>{birthdayCount} birthday today</h1>
 
-            <div className="user-card">
-                <div className="user-profile font-500">T</div>
+            {
+                listOfUsers.length === 0 ? (
+                    <p className='font-500'>No Birthdays today</p>
+                ):(
+                    listOfUsers.map((user, index) => (
+                        user.isBirthday ? (
+                            <div className="user-card" key={index}>
+                                <div className="user-profile font-500">{user.profile}</div>
 
-                <div className="user-info">
-                    <strong className="header font-700">Test Name</strong>    
-                    <p className="sub-header font-400">29 years</p>
-                </div>
-            </div>
+                                <div className="user-info">
+                                    <strong className="header font-700">{user.name}</strong>
+                                    <p className="sub-header font-400">{user.age} years</p>
+                                </div>
+                            </div>
+                        ): user.isBirthday
+                    ))
+                )
+            }
+
+                
             
             <button className="clear-btn font-500" onClick={() => hideWrapper()}>Add Birthday</button>
-            <button className="clear-btn font-500">Clear All</button>
+            <button className="clear-btn font-500" onClick={() => clearAll()}>Clear All</button>
 
         </div>
     )
